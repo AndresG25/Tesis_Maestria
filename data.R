ec4cc45d5c9a0fa001340a621fce44cd714090df
AndresG25 <andres25@utp.edu.co>
2024-02-03 17:13:19 -0500

    Código para actualizar la BD en formato apto para shiny

---
+++ data.R
@@ -0,0 +1,20 @@
+library(RMySQL)
+library(tidyverse)
+library(lubridate)
+
+#Este código actualiza la base de datos que será subida a Github para la lectura de la shinyapp.
+
+while (TRUE) {
+
+database<- dbConnect(MySQL(), user="root", host="127.0.0.1", password="", dbname="estacion")
+
+query<- dbGetQuery(database,statement ="SELECT * FROM dataestacion")
+
+query1 <- query %>% mutate(Fecha = parse_date_time(Fecha, "ymd HMS"))
+
+write.csv(query1, file="C:/Users/USUARIO/Desktop/MAESTRIA/Tesis-Maestria/NuevaPrueba/data1.csv")
+
+dbDisconnect(database)
+
+#Sys.sleep(60)  # Pausa el script durante 60 segundos
+}
