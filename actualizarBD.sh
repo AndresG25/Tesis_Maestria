bc42771f4506f2bd424fb2606f915674da11448f
AndresG25 <andres25@utp.edu.co>
2024-02-03 22:05:08 -0500

    nuevos datos

--- actualizarBD.sh
+++ actualizarBD.sh
@@ -1,8 +1,4 @@
-
-cd C:/Users/USUARIO/Desktop/Tesis_Maestria
-git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -am "Nuevo commit"
-git push origin main
+system("cd C:/Users/USUARIO/Desktop/Tesis_Maestria")
+system("git add --all")
+system("git commit -am 'Nuevo commit'")
+system("git push origin main")
