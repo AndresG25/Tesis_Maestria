1d26bea2444a4d6f0b6e2443a4270334d2bdd97d
AndresG25 <andres25@utp.edu.co>
2024-02-22 23:01:02 -0500

    Nuevo commit

--- app1.R
+++ app1.R
@@ -316,7 +316,7 @@ server <- function(input, output, session) {
 
 ui <- dashboardPage(
   dashboardHeader(title = "Sistema de Datos - Generador Eólico Axial",
-                  titleWidth = 450), rightUi = tags$li(class = "dropdown",
+                  titleWidth = 450, rightUi = tags$li(class = "dropdown",
                                              tags$img(src = "UTP.png", height = "50px", style = "float: right; margin-right: 10px; margin-top: 10px;"))),
     
   dashboardSidebar(
