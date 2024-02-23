af8b9d2144b0d883a7451c9da08073e8d7eda866
AndresG25 <andres25@utp.edu.co>
2024-02-23 09:10:03 -0500

    Actualización de datos 2024-02-23 09:10:03

--- actualizarBD.sh
+++ actualizarBD.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd C:/Users/USUARIO/Desktop/Tesis_Maestria
 git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -am "Nuevo commit"
+
+commit_message="Actualización de datos $(date +'%Y-%m-%d %H:%M:%S')"
+
+git commit -m "$commit_message"
+
 git push origin main
